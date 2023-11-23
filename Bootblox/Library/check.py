9b6c5f63026d061e6186d9d00dff86da0367be5a
Bo <bo.van.laarhoven@gmail.com>
2023-11-23 16:43:35 +0100

    iki

---
+++ Bootblox/Library/check.py
@@ -0,0 +1,19 @@
+import os
+
+Roblox = "/Applications/Roblox.app"
+ClientSettings = Roblox + "/Contents/MacOS/ClientSettings"
+ClientSettingsFile = ClientSettings + "/ClientSettings.json"
+
+def check():
+    if os.path.exists(ClientSettings):
+        print("ClientSettings Found")
+    else:
+        print("ClientSettings Not Found")
+        print("Creating Folder...")
+        os.makedirs(ClientSettings, exist_ok=True)
+        print("Creating File...")
+        with open(ClientSettingsFile, "w") as f:
+            f.write("{}")
+
+
+check()
---
+++ main.py
@@ -0,0 +1,5 @@
+from Bootblox.Library import check
+from Bootblox.UI import window
+
+check.check()
+window.window()
