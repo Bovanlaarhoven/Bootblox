87a0e42e21f4011081ee8728ce9952f1d76631fb
Bo <bo.van.laarhoven@gmail.com>
2023-11-27 01:24:12 +0100

    Update window.py

--- src/Bootblox/UI/window.py
+++ src/Bootblox/UI/window.py
@@ -24,32 +24,6 @@ def set_fps(event):
     except ValueError:
         print("Invalid input. Please enter a valid integer for FPS.")
 
-
-def set_notifcation_mode(*args):
-    value = notifcation_mode.get()
-    if value == "GameInfo":
-        send_notification("GameInfo", {"game_id": get_game_id()})
-    elif value == "ServerInfo":
-        send_notification("ServerInfo", get_server_location_info())
-
-notification_sent = False
-
-def toggle_notification():
-    global notification_sent
-    value = notification_var.get()
-    join = "[FLog::Output] ! Joining game"
-    leave = "[FLog::Network] Time to disconnect replication data"
-
-    join_logs = get_latest_log(join)
-    leave_logs = get_latest_log(leave)
-
-    if value and join_logs and (not notification_sent):
-        send_notification("GameInfo", {"game_id": get_game_id()})
-        notification_sent = True
-    elif not value or not join_logs:
-        notification_sent = False
-
-
 def set_rendering(*args):
     value = rendering_mode.get()
     rendering(value)
@@ -102,27 +76,6 @@ def create_tab(tab_name, content):
         lighting = tk.OptionMenu(frame3, lighting_mode, "Voxel", "ShadowMap", "Future")
         lighting.pack(side="right", fill="x", expand=True)
         lighting_mode.trace("w", set_lighting)
-    if tab_name == "Intergration":
-        frame1 = tk.Frame(tab, borderwidth=1, relief="solid", padx=5, pady=5)
-        frame2 = tk.Frame(tab, borderwidth=1, relief="solid", padx=5, pady=5)
-        frame1.pack(fill="x", pady=5)
-        frame2.pack(fill="x", pady=5)
-
-        label_notifcation = tk.Label(frame1, text="Enable notifcations:", anchor=tk.W)
-        label_notifcation.pack(side="left")
-        label_notifcation_mode = tk.Label(frame2, text="notifcation mode :", anchor=tk.W)
-        label_notifcation_mode.pack(side="left")
-
-        # notifcation
-        global notification_var
-        global notifcation_mode
-        notification_var = tk.BooleanVar(tab)
-        integration_toggle = tk.Checkbutton(frame1, variable=notification_var, command=toggle_notification)
-        integration_toggle.pack(side="right")
-        notifcation_mode = tk.StringVar(tab)
-        notifcation = tk.OptionMenu(frame2, notifcation_mode, "ServerInfo", "GameInfo")
-        notifcation.pack(side="right", fill="x", expand=True)
-        notifcation_mode.trace("w", set_notifcation_mode)
 
 
 window = tk.Tk()
